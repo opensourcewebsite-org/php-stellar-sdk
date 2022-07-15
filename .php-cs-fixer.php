88847c7755f8f3c3a4dcbcd82b142881d802b69f
Vitaliy Shevchenko <grandmotivator@gmail.com>
2022-07-15 19:10:40 +0700

    update

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,27 +1,27 @@
 <?php
 
 # https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/doc/usage.rst
+# https://mlocati.github.io/php-cs-fixer-configurator/#version:3.9
+
 declare(strict_types=1);
 
-$finder = PhpCsFixer\Finder::create()
-    ->in([
-        'src',
-        'tests',
-    ])
-;
+$config = new PhpCsFixer\Config();
 
 /*
  * Available rules: https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/doc/rules/index.rst
  * Available sets: https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/doc/ruleSets/index.rst
  */
 
-$config = new PhpCsFixer\Config();
-
 return $config
-    ->setFinder($finder)
     ->setRiskyAllowed(true)
     ->setRules([
         '@PSR12' => true, // PSR12 coding style: https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/doc/ruleSets/PSR12.rst
         '@PSR12:risky' => true, // PSR12Risky coding style: https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/doc/ruleSets/PSR12Risky.rst
         '@DoctrineAnnotation' => true, // Format Doctrine annotations: https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/doc/ruleSets/DoctrineAnnotation.rst
-    ]);
+        'ordered_imports' => true,
+    ])
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->exclude('vendor')
+            ->in(__DIR__)
+    );
